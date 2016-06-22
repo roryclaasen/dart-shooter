9a0cc70d7a8bca94ab9a8dbc45ff472a6392e240
GOGO98901 <gogo98901@live.co.uk>
2016-06-22 22:46:52 +0100

    yes its broken

--- scripts/write-data-after-build.py
+++ scripts/write-data-after-build.py
@@ -1,25 +1,22 @@
-import PyYAML
 import os
+import shutil
+
+def copy_rename(old_file_name, new_file_name):
+        src_dir = os.curdir
+        dst_dir = os.path.join(os.curdir, "subfolder")
+        src_file = os.path.join(src_dir, old_file_name)
+        shutil.copy(src_file, dst_dir)
+
+        dst_file = os.path.join(dst_dir, old_file_name)
+        new_dst_file_name = os.path.join(dst_dir, new_file_name)
+        os.rename(dst_file, new_dst_file_name)
 
 path = os.path.dirname(os.path.realpath(__file__))
 path = path[:-len("scripts")]
 
-pubspec = {}
-with open(path + "pubspec.yaml")  as stream:
-	pubspec = yaml.load(stream)
-
-data = dict(
-    name = pubspec['name'],
-    description = pubspec['description'],
-	version = pubspec['version'],
-	homepage = pubspec['homepage']
-)
-
-with open(path + 'build/web/data.yml', 'w') as outfile:
-    outfile.write(yaml.dump(data, default_flow_style=False))
+copy_rename(path + "pubspec.yaml", path +  "build/web/data.yaml")
 
 with open(path + 'build/web/README.md', 'w') as outfile:
-	outfile.write('# Dart Shooter ' + data['version'] + '\n')
-	outfile.write(data['description'] + '\n\n')
+	outfile.write('# Dart Shooter\n\n')
 	outfile.write('This branch gets updated automatically!\n\n')
 	outfile.write('Any change or commit will be **overwritten**!\n')
